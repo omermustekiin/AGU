dbd04c3a69f31dd176fd23b2298a72e41689fae6
msahin <1903013035@student.beykent.edu.tr>
2023-03-04 20:31:26 +0300

    First commit

---
+++ agu-api/src/main/java/com/beykent/aguapi/AguApiApplication.java
@@ -0,0 +1,13 @@
+package com.beykent.aguapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguApiApplication.class, args);
+	}
+
+}
---
+++ agu-api/src/test/java/com/beykent/aguapi/AguApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beykent.aguapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AguApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
